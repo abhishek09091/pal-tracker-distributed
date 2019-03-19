a5f13a1a0d8aace61c3b8a6128de5bc14d916361
abhishek09091 <abhishek.09091@gmail.com>
2019-03-19 15:12:56 +0530

    Resolved build failure

--- components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
+++ components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
@@ -22,7 +22,9 @@ public class ProjectClient {
 
     @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo projectInfo = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        projectsCache.put(projectId,projectInfo);
+        return projectInfo;
     }
 
     public ProjectInfo getProjectFromCache(long projectId) {
